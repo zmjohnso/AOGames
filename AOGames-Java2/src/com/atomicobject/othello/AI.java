ff37b8f721dc6d43410280868c8c06771d1e6242
zmjohnso <zmjohnso@mtu.edu>
2020-10-31 13:48:41 -0400

    Add files via upload

---
+++ AOGames-Java2/src/com/atomicobject/othello/AI.java
@@ -0,0 +1,17 @@
+package com.atomicobject.othello;
+
+import java.util.Arrays;
+import java.util.ListIterator;
+
+public class AI {
+
+	public AI(int[] moves) {
+		
+	}
+
+	// Github test
+	public int computeMove(GameState state) {
+		System.out.println("AI returning canned move for game state - " + state);
+		return 1;
+	}
+}
